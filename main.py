5996f2f77d2f3d8ad6fa26bcbb8cfd2c61261ead
rozium <fahrurrozi31@gmail.com>
2019-02-13 07:41:23 +0700

    add base audio steganography

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# TODO: Serve using Flask
+'''
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return jsonify({'test': 'lol'})
+
+if __name__ == '__main__':
+  app.run(debug=True)
+'''
+
+
+# Audio Steganography
+
+import wave
+f = wave.open('./example/wav/1.wav', 'rb')
+fb = bytearray(list(f.readframes(f.getnframes())))
+
+# embed
+s = 'secret message'
+s = s + int((len(fb)-(len(s)*8*8))/8) * '#'
+bs = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in s])))
+for i, b in enumerate(bs):
+  fb[i] = (fb[i] & 254) | b
+fm = bytes(fb)
+
+# extract
+lsb = [fb[i] & 1 for i in range(len(fb))]
+s = ''.join(chr(int(''.join(map(str,lsb[i:i+8])),2)) for i in range(0,len(lsb),8)).split('###')[0]
+print s
